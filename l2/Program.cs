0ea307335622bfd1c5206105218311bf7b707f6b
Neytllas <looopit@gmail.com>
2022-03-11 19:51:49 +0800

    Добавьте файлы проекта.

---
+++ l2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace factor
+{
+    class Program
+    {
+        static long Ffactorial(int n)
+        {
+            long res = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
+
+        static long Rfactorial(int n)
+        {
+            return n < 1 ? 1 : n * Rfactorial(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Ffactorial(6));
+            Console.WriteLine(Rfactorial(3));
+
+            Console.ReadLine();
+        }
+    }
+}
